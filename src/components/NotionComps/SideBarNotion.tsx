44af94347eec3ddaf869465ddcd766284b546301
mawnir <mawnirfas@gmail.com>
2024-12-02 20:53:11 +0100

    fix z index

--- src/components/NotionComps/SideBarNotion.tsx
+++ src/components/NotionComps/SideBarNotion.tsx
@@ -123,7 +123,7 @@ const SideBarNotion = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-screen bg-[#f5f5f5] overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          "group/sidebar h-screen bg-[#f5f5f5] overflow-y-auto relative flex w-60 flex-col",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
